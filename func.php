0c192836a2747814264334533dc039273a1a4686
belalammar <35145666+belalammar@users.noreply.github.com>
2018-04-13 16:03:57 +0200

    Create func.php

---
+++ func.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+</head>
+<body>
+<div id="demo"></div>
+	<script type="text/javascript">
+	document.getElementById("demo").innerHTML=Math.floor(-1/2);
+	</script>
+</body>
+</html>
