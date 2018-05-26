625553fb11c67ba13f88e5cb5ae97241058c8e19
belalammar <35145666+belalammar@users.noreply.github.com>
2018-05-26 18:45:23 +0200

    Create signin.php

---
+++ signin.php
@@ -0,0 +1,13 @@
+<?php
+include("class/users.php");
+$signin=new users;
+extract($_POST);
+if($signin->signin($e,$p))
+{
+	$signin->url("home.php");
+}
+else
+{
+	$signin->url("index.php?run=failed");
+}
+?>
