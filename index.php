22df8d108ec80341a42c7a3a0facd492479503d8
belalammar <35145666+belalammar@users.noreply.github.com>
2018-05-26 18:38:03 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,213 +1,155 @@
+<?php
+include "../class/users.php";
+$cat=new users;
+$category=$cat->cat_shows();
+//print_r($category);
+?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <title>singin and signup</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="css/bootstrap.min.css">
-  <style>
-  body {font-family: Arial, Helvetica, sans-serif;}
-* {box-sizing: border-box}
-/* Full-width input fields */
-input[type=text], input[type=password] {
-    width: 100%;
-    padding: 15px;
-    margin: 5px 0 22px 0;
-    display: inline-block;
-    border: none;
-    background: #f1f1f1;
-}
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
 
-/* Add a background color when the inputs get focus */
-input[type=text]:focus, input[type=password]:focus {
-    background-color: #ddd;
-    outline: none;
-}
+    <title>  لوحه التحكم </title>
 
-/* Set a style for all buttons */
-button {
-    background-color: #4CAF50;
-    color: white;
-    padding: 14px 20px;
-    margin: 8px 0;
-    border: none;
-    cursor: pointer;
-    width: 100%;
-    opacity: 0.9;
-}
+    <!-- Bootstrap core CSS -->
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
 
-button:hover {
-    opacity:1;
-}
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link href="..//css/ie10-viewport-bug-workaround.css" rel="stylesheet">
 
-/* Extra styles for the cancel button */
-.cancelbtn {
-    padding: 14px 20px;
-    background-color: #f44336;
-}
+    <!-- Custom styles for this template -->
+    <link href="../css/dashboard.css" rel="stylesheet">
 
-/* Float cancel and signup buttons and add an equal width */
-.cancelbtn, .signupbtn {
-  float: left;
-  width: 50%;
-}
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="../../assets/js/ie-emulation-modes-warning.js"></script>
 
-/* Add padding to container elements */
-.container {
-    padding: 16px;
-}
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
 
-/* The Modal (background) */
-.modal {
-    display: none; /* Hidden by default */
-    position: fixed; /* Stay in place */
-    z-index: 1; /* Sit on top */
-    left: 0;
-    top: 0;
-    width: 100%; /* Full width */
-    height: 100%; /* Full height */
-    overflow: auto; /* Enable scroll if needed */
-    background-color: #474e5d;
-    padding-top: 50px;
-}
+  <body>
 
-/* Modal Content/Box */
-.modal-content {
-    background-color: #fefefe;
-    margin: 5% auto 15% auto; /* 5% from the top, 15% from the bottom and centered */
-    border: 1px solid #888;
-    width: 80%; /* Could be more or less, depending on screen size */
-}
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a style="float:right;margin-left:600px;" class="navbar-brand" href="#"> الامتحانات الاونلاين  </a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav navbar-left">
+            <li><a href="#">  لوحه التحكم </a></li>
+            <li><a href="setting.php"> الاعدادت </a></li>
+            <li><a href="profile.php"> الصفحه الشخصيه </a></li>
+            <li><a href="help.php">  المساعده </a></li>
+          </ul>
+          <form class="navbar-form navbar-left">
+            <input type="text" class="form-control" placeholder="Search...">
+          </form>
+        </div>
+      </div>
+    </nav>
 
-/* Style the horizontal ruler */
-hr {
-    border: 1px solid #f1f1f1;
-    margin-bottom: 25px;
-}
- 
-/* The Close Button (x) */
-.close {
-    position: absolute;
-    right: 35px;
-    top: 15px;
-    font-size: 40px;
-    font-weight: bold;
-    color: #f1f1f1;
-}
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-sm-3 col-md-2 sidebar">
+          <ul class="nav nav-sidebar">
+            <li class="active"><a href="#">Overview <span class="sr-only">(current)</span></a></li>
+            <li><a href="#">  صفحه التقارير </a></li>
+            <li><a href="#"> صفحه التحليل </a></li>
+            <li><a href="#"> صفحه الاختبار</a></li>
+          </ul>
+          </div>
+          
+        <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+          <h1 class="page-header"> لوحه التحكم </h1>
 
-.close:hover,
-.close:focus {
-    color: #f44336;
-    cursor: pointer;
-}
+          <div class="table-responsive">
+            <table class="table table-striped">
+			<?php
+			if(isset($_GET['msg']) && !empty($_GET['msg']))
+			{
+				echo "<p> تم ادخال السؤال بنجاح </p>";
+			}
+			
+			?>
+			
+			
+				  <form method="post" action="add_quiz.php">
+					<div class="form-group">
+					  <label for="text"> السؤال </label>
+					  <input type="text" class="form-control" name="qus"  placeholder="Enter question">
+					</div>
 
-/* Clear floats */
-.clearfix::after {
-    content: "";
-    clear: both;
-    display: table;
-}
-
-/* Change styles for cancel button and signup button on extra small screens */
-@media screen and (max-width: 300px) {
-    .cancelbtn, .signupbtn {
-       width: 100%;
-    }
-}
-</style>
-</head>
-<body>
-<br>
-<br>
-<div class="container">
-	<div class="row">
-		<div class="col-sm-12">
-			 <div class="panel panel-danger">
-				  <div class="panel-heading"> الامتحانات عبر الانترنت </div>
-				 
-		</div>
-    </div>
-</div>
-
-
-
-
-	<div class="container">
-		<div class="row">
-			     <div class="col-sm-6">
-					  <div class="panel panel-info">
-                      <div class="panel-heading"> تسجيل الدخول </div>
-						<div class="panel-body">
-						<?php
-						if(isset($_GET['run'])  && $_GET['run']=="failed")
-						{
-							echo "Your email or password is not correct";
-						}
-						
-						
-						?>
-      <form role="form" action="home.php" method="post">
-							  
-							  <div class="container">
-    <label for="uname"><b> الاسم </b></label>
-    <input type="text" placeholder="Enter Username" name="uname" required>
-
-    <label for="psw"><b> الباسورد </b></label>
-    <input type="password" placeholder="Enter Password" name="psw" required>
-
-    <button type="submit"> دخول </button>
-    <label>
-      <input type="checkbox" checked="checked" name="تذكر"> Remember me
-    </label>
-  </div>
-
-  <div class="container" style="background-color:#f1f1f1">
-    <button type="button" class="cancelbtn"> الغاء </button>
-    <span class="psw">نسيت  <a href="#">كلمه  السر ?</a></span>
-  </div>
-</form>
-							  
+					<div class="form-group">
+					  <label for="text"> الاختيار 1</label>
+					  <input type="text" class="form-control"  name="op1"  placeholder="Enter option-1">
+					</div>
+					<div class="form-group">
+					  <label for="text"> الاختيار2</label>
+					  <input type="text" class="form-control" name="op2"  placeholder="Enter option-2">
+					</div>
+					
+					<div class="form-group">
+					  <label for="text"> الاختيار 3</label>
+					  <input type="text" class="form-control"  name="op3"  placeholder="Enter option-3">
+					</div>
+					
+					<div class="form-group">
+					  <label for="text">الاختيار4</label>
+					  <input type="text" class="form-control"  name="op4" id="email" placeholder="Enter option-4">
+					</div>
+					<div class="form-group">
+					  <label for="text"> الاجابه </label>
+					  <input type="text" class="form-control" name="ans" id="email" placeholder="Enter answer">
+					</div>
+						<div class="form-group">
+						   
+						   <select class="form-control" id="sel1" name="cat">
+						   
+								<option value="" disabled>  اختيار القسم </option>
+								<?php
+								foreach($category as $c)
+								{
+									echo "<option value=".$c['id'].">".$c['cat_name']."</option>";
+								}
 								
-				   <div class="col-sm-6">
-							<div class="panel panel-info">
-								
-								<div class="panel-heading"> تسجيل الخروج </div>
-							<div class="panel-body">
-							<?php 
-							if(isset($_GET['run'])&& $_GET['run']=="success")
-							{
-								echo "<mark>Your registration successfully done</mark>";
-							} 
-							?>
-							  <form role="form" method="post" action="signup_sub.php" enctype="multipart/form-data"  >
-							  	<div class="form-group">
-								  <label for="name"> الاسم :</label>
-								  <input type="text" class="form-control" name="n" id="name" placeholder="Enter name">
-								</div>
-								<div class="form-group">
-								  <label for="email">الايميل  :</label>;
-								  <input type="email" class="form-control" name="e" id="email" placeholder="Enter email">
-								</div>
-								<div class="form-group">
-								  <label for="pwd">الباسورد :</label>
-								  <input type="password" class="form-control" name="p" id="pwd" placeholder="Enter password">
-								</div>
-								<div class="form-group">
-								  <label for="pwd"> رفع  صوره  لك </label>
-								  <input type="file" class="form-control" name="img" >
-								</div>
-								<button type="submit" class="btn ظ">
-								    دخول 
-								</button>
-							  </form>
-						  </div>
-						  </div>
-			      </div>
-	   </div>
-    </div>
-            </div></div></div></div>
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
-  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-        
-</body>
+								?>								
+						  </select>
+						</div>
+					<button type="submit" class="btn btn-default"> ادخال </button><br>
+				  </form>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+  
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="../js/bootstrap.min.js"></script>
+    <!-- Just to make our placeholder images work. Don't actually copy the next line! -->
+    <script src="../js/vendor/holder.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="../js/ie10-viewport-bug-workaround.js"></script>
+  </body>
 </html>
+
