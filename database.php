320ea33e5e6163bfc8671558a09cc88d35f60bd7
belalammar <35145666+belalammar@users.noreply.github.com>
2018-10-02 21:26:21 +0200

    Update database.php

--- database.php
+++ database.php
@@ -1,29 +1,36 @@
-CREATE database  exam;
 
 
+-- Database: `quiz`
+--
 
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `category`
+--
 
 CREATE TABLE `category` (
   `id` int(4) NOT NULL,
   `cat_name` varchar(40) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf-8;
 
-
+--
 -- Dumping data for table `category`
-
+--
 
 INSERT INTO `category` (`id`, `cat_name`) VALUES
-(1, 'computer science'),
-(2, 'system information'),
-(3, 'accounting managment'),
-(4, 'sociology');
+(1, 'php'),
+(2, 'html'),
+(3, 'css'),
+(4, 'javascript'),
+(5, 'jquery'),
+(6, 'bootstrap');
 
 -- --------------------------------------------------------
 
-
+--
 -- Table structure for table `questions`
-
+--
 
 CREATE TABLE `questions` (
   `id` int(11) NOT NULL,
@@ -36,20 +43,25 @@ CREATE TABLE `questions` (
   `cat_id` int(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf-8;
 
-
+--
+-- Dumping data for table `questions`
+--
 
 INSERT INTO `questions` (`id`, `question`, `ans1`, `ans2`, `ans3`, `ans4`, `ans`, `cat_id`) VALUES
+(1, ' What does PHP stand for?', 'PHP: Hypertext Preprocessor', 'Private Home Page', 'Personal Hypertext Processor', 'none of them', 0, 1),
+(2, 'How do you write "Hello World" in PHP', 'echo "Hello World";', '"Hello World";', ' Document.Write("Hello World");', 'none of them', 0, 1),
+(3, ' The PHP syntax is most similar to:', 'Perl and C', 'VBScript', 'JavaScript', 'none of them', 0, 1),
+(4, 'When using the POST method, variables are displayed in the URL:', 'true', 'false', 'none of them', 'return 1', 1, 1),
+(5, 'PHP server scripts are surrounded by delimiters, which?', '&lt;?php...?&gt;', '&lt;?php&gt;...&lt;/?&gt;', '&lt;&amp;&gt;...&lt;/&amp;&gt;', '&lt;script&gt;...&lt;/script&gt;', 0, 1),
+(6, 'All variables in PHP start with which symbol?', '@', '%', '$', '&amp;', 2, 1),
+(7, '', '', '', '', '', 0, 0),
+(8, 'What is the correct way to end a PHP statement?', '.', ';', 'new line', '&lt;php&gt;', 1, 1);
 
+-- --------------------------------------------------------
 
-
-
-
-
-
-
-
-
+--
 -- Table structure for table `signup`
+--
 
 CREATE TABLE `signup` (
   `id` int(11) NOT NULL,
@@ -59,44 +71,56 @@ CREATE TABLE `signup` (
   `img` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf-8;
 
+--
+-- Dumping data for table `signup`
+--
 
+INSERT INTO `signup` (`id`, `name`, `email`, `pass`, `img`) VALUES
+(1, 'danish', 'dsaifi007@gmail.com', '1234', 'C:xampp	mpphpF0CB.tmp'),
+(5, 'aamir', 'amir123@gmail.com', '88264', 'C:xampp	mpphpC01D.tmp'),
+(6, 'aamir', 'dsaifi007@gmail.com', '88888', 'cricket-stadium.jpg'),
+(7, 'aamir', 'amir123@gmail.com', '888888', 'cricket-stadium.jpg'),
+(8, '', '', '', ''),
+(9, 'abcd', 'abcd@gmail.com', '123456', 'IMG_20160505_195500_HDR.jpg');
 
+--
+-- Indexes for dumped tables
+--
+
+--
 -- Indexes for table `category`
-
+--
 ALTER TABLE `category`
+  ADD PRIMARY KEY (`id`);
 
-    ADD PRIMARY KEY (`id`);
-
-
+--
 -- Indexes for table `questions`
-
+--
 ALTER TABLE `questions`
   ADD PRIMARY KEY (`id`);
 
-
+--
 -- Indexes for table `signup`
-
+--
 ALTER TABLE `signup`
   ADD PRIMARY KEY (`id`);
 
-
+--
 -- AUTO_INCREMENT for dumped tables
+--
 
- AUTO_INCREMENT for table `category`
-
+--
+-- AUTO_INCREMENT for table `category`
+--
 ALTER TABLE `category`
-
-    MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+--
 -- AUTO_INCREMENT for table `questions`
-
-
--- ALTER TABLE `questions`
-  
+--
+ALTER TABLE `questions`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 --
 -- AUTO_INCREMENT for table `signup`
-
+--
 ALTER TABLE `signup`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
-  
